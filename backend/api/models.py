f88cd90d9306f8802c4c7f5e99c1a75fcacae3fc
Zwane71 <makhohlisatemba@gmail.com>
2026-08-13 08:34:12 +0200

    Creating the foundation of the backend to create a RBAC

---
+++ backend/api/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        Admin = 'ADMIN', 'Admin',
+        Technician = 'TECHNICIAN', 'Technician',
+        Client = 'CLIENT', 'Client',
+
+    role = models.CharField(
+
+        max_length=20,
+        choices=Role.choices,
+        default=Role.Client,
+    )
+
+    def __str__(self):
+        return f"{self.username} ({self.get_role_display()})"
